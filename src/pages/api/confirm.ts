e4e7d37d8a79dcbac2074e3898cff323f8725139
Addison <addison@gitbook.com>
2023-08-16 16:27:15 +0200

    update

--- src/pages/api/confirm.ts
+++ src/pages/api/confirm.ts
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// @ts-nocheck
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 
